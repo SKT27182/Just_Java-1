1ed7fa9986374b897c9aec79b2797da41381e350
Shailja Kant Tiwari <smartshailjakant1998@gmail.com>
2021-07-23 11:53:57 +0530

    Added a Gmail_Intent, Phone_Intent and a Map_Intent

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -21,7 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    int quantity = 0;
+    int quantity = 1;
+    String name="";
 
     /**
      * This method is called when the order button is clicked.
@@ -34,10 +38,53 @@ public class MainActivity extends AppCompatActivity {
         CheckBox CheckChocolate = (CheckBox) findViewById(R.id.isCheckedChoco);
         boolean HasChoco = CheckChocolate.isChecked();
 
-        int Price = CalculatePrice(quantity);
+        int Price = CalculatePrice(quantity,HasCream, HasChoco);
+
         String Summary = OrderSummary(Price, HasCream, HasChoco);
-        display(quantity);
-        displayPrice(Summary);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee Order Place by " + name);
+        intent.putExtra(Intent.EXTRA_TEXT, Summary);
+        if(intent.resolveActivity(getPackageManager()) !=null){
+            startActivity(intent);
+        }
+
+       display(quantity);
+
+    }
+
+
+    /**
+     * This method is called when Call Button is clicked
+     */
+
+    public void Call(View view){
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + "7607135731"));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * This method is called when MAP Button is clicked
+     */
+
+    public void Map(View view){
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.setData(Uri("geo:47.6,-122.3"));
+//        if (intent.resolveActivity(getPackageManager()) != null) {
+//            startActivity(intent);
+//        }
+
+        Uri gmmIntentUri = Uri.parse("geo:26.2207102,81.2281093");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
+
     }
 
 
@@ -45,8 +92,18 @@ public class MainActivity extends AppCompatActivity {
      * Method for calculating Price
      */
 
-    private int CalculatePrice(int number) {
-        return number * 5;
+    private int CalculatePrice(int number,boolean HasCream, boolean HasChoco) {
+        int basePrice=5;
+
+        if(HasCream){
+            basePrice++;
+        }
+
+        if(HasChoco){
+            basePrice +=2;
+        }
+
+        return number * basePrice;
     }
 
 
@@ -55,13 +112,14 @@ public class MainActivity extends AppCompatActivity {
      */
 
     private String OrderSummary(int price, boolean HasCream, boolean HasChoco) {
-        EditText Txt =  findViewById(R.id.plain_text_input);
-        String Temp=Txt. getText(). toString();
-        if (!HasChoco & !HasCream) {
-            return "$" + price;
-        } else {
-            return "NAME: "+ Temp + "\nAdd Whipped Cream?" + HasCream + "\nAdd Chocolate?" + HasChoco + "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank You!";
-        }
+        /**
+         * This fetch the text from the EditText and store it to a Temp variable
+         */
+        EditText Txt = findViewById(R.id.plain_text_input);
+        name = Txt.getText().toString();
+
+        return "NAME: " + name + "\nAdd Whipped Cream?" + HasCream + "\nAdd Chocolate?" + HasChoco + "\nQuantity: " + quantity + "\nTotal: $" + price + "\nThank You!";
+
     }
 
     /**
@@ -76,15 +134,19 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price value on the screen.
      */
 
-    private void displayPrice(String Summary) {
-        TextView quantityTextView = (TextView) findViewById(R.id.price);
-        quantityTextView.setText(Summary);
-    }
+//    private void displayPrice(String Summary) {
+//        TextView quantityTextView = (TextView) findViewById(R.id.price);
+//        quantityTextView.setText(Summary);
+//    }
 
 
     /* This method is called when the increment button is clicked*/
 
     public void increment(View view) {
+        if(quantity==10) {
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_LONG).show();
+            return;
+        }
         quantity++;
         displayIncDcr(quantity);
     }
@@ -94,6 +156,10 @@ public class MainActivity extends AppCompatActivity {
     /* This method is called when the decrement button is clicked*/
 
     public void decrement(View view) {
+        if (quantity==1) {
+            Toast.makeText(this, "You cannot have less than 1 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity--;
         displayIncDcr(quantity);
     }
